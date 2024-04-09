15b8d92892c4ddfb745cbf33e61f8978b02ef811
eyubSalihOzdemir <eyupsalih776@gmail.com>
2024-04-09 03:26:38 +0200

    added dark mode image functionality

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,11 +1,18 @@
-import irobotlogo from "../assets/i-robot-logo.svg";
+import iRobotLogo from "../assets/i-robot-logo.png";
+import iRobotLogoWhite from "../assets/i-robot-logo-white.png";
+import { useStateContext } from "../contexts/ContextProvider.tsx";
 
 function Home() {
+  const { isDarkMode } = useStateContext();
+
   return (
     <div className="flex h-screen flex-col">
       <div className="flex justify-center">
-        {/* <img src={irobotlogo} alt="logo" sizes="" /> */}
-        <img src={irobotlogo} alt="SVG Logo Image" className="size-48"></img>
+        <img
+          src={isDarkMode ? iRobotLogoWhite : iRobotLogo}
+          alt="SVG Logo Image"
+          className="size-48 fill-lime-500"
+        ></img>
       </div>
       <div className="lex-col flex grow items-center justify-center font-medium">
         Currently under development...
